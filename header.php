361137e24d9e66d24ae5e76c569e51d617ca671d
reggie duran <reggieduran15@gmail.com>
2015-06-19 15:04:42 +0800

    inner page design done

--- header.php
+++ header.php
@@ -68,7 +68,14 @@ if ( ! empty( $headerImageUrl ) ) {
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'russell' ); ?></a>
 
     <header id="masthead" class="site-header" role="banner">
-		
+		<?php
+		    if ( is_page() || is_single() ) { ?>
+        		<div class="site-branding">
+        			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+        			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+        		</div><!-- .site-branding -->
+    	<?php } ?> 
+    	
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'russell' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
