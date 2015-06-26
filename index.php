1c9da7ff4a8fe594939d2f48589ca26008f3aaf8
reggie duran <reggieduran15@gmail.com>
2015-06-26 17:02:44 +0800

    site logo fixed

--- index.php
+++ index.php
@@ -15,8 +15,10 @@ get_header(); ?>
 
 <div class="russell_left_content">
 	<div class="site-branding">
-		<?php wp_head(); ?>
-		<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+		<?php //wp_head();    
+		    russell_feature_logo();
+		 ?>
+    		<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 	</div><!-- .site-branding -->
     
     <div class="site-elaboration">
