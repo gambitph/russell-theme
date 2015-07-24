51c96e14f3fa53596dfc0207f14e20ef9d29fc81
Benjamin Intal <bfintal@gmail.com>
2015-07-24 17:38:13 +0800

    Adjusted search template & post meta

--- search.php
+++ search.php
@@ -5,29 +5,30 @@
  * @package russell
  */
 
-get_header(); ?>
-<div class="russell-search-left-content">
+get_header();
+?>
+<div id="content" class="russell-content-wrapper russell-content-reverse">
+	<section class="russell-content-large russell-content-image"></section>
+	
+	<section class="russell-content-small russell-content-area">
+		<div>
+			<h1 class="russell-site-title">
+				<a href='#'><?php echo get_search_query() ?></a>
+				<span><?php _e( 'Search', 'russell' ) ?></span>
+			</h1>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		</div>
+		<div>
+			<?php if ( have_posts() ) : ?>
+			<?php russell_selected_post_tags(); ?>
+			<?php endif; ?>
 
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( __( '%s', 'russell' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-    			<span><?php printf( __( 'Search') ); ?></span>
-			</header><!-- .page-header -->
-            
-            <?php russell_selected_post_tags(); ?>
-		
-		<?php endif; ?>
-		</main><!-- #main -->
-		<?php //get_sidebar(); ?>
-        
+		</div>
         <div class="russell-copyright">
             <?php russell_copyright(); ?>
         </div>
-	</section><!-- #primary -->
-
+	</section>
 </div>
-<?php get_footer(); ?>
+<?php
+
+get_footer();
