f48cdf8ef3be1ef091afbde140f783d395f70ec4
Benjamin Intal <bfintal@gmail.com>
2015-07-24 18:14:31 +0800

    Simplified single.php

--- single.php
+++ single.php
@@ -10,57 +10,46 @@ get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
 
 <div id="content" class="russell-content-wrapper">
-
-    <?php get_template_part( 'content', 'featured-image' );
-	
-	if ( has_post_thumbnail() ) {
-    	$imageAttachment = wp_get_attachment_image_src( get_post_thumbnail_id(), 'russell-featured-image' );
-
-    	if ( ! empty( $imageAttachment ) ) {
-    	    $contentClass = "russell-content-small";
-    		?>
-            <section class="<?php $contentClass ?> russell-content-area">
-                <div>
-            <?php
-    	}
-    } else {
-        $contentClass = "russell-content-full"
-        ?>
-        <section class="<?php echo esc_attr( $contentClass ); ?> russell-content-area">
-            <div>
-        <?php
-    }
-    
-    ?>
-    
-	<?php get_template_part( 'content', 'single' ); ?>
-
-		<?php russell_post_nav(); ?>
-
-		<?php
-			// If comments are open or we have at least one comment, load up the comment template
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
-		?>
-
-    <?php get_sidebar(); ?>
-    
-    <div class="russell-copyright">
-        <?php russell_copyright(); ?>
-    </div>
-    
     <?php
-    if ( has_post_thumbnail() ) {
+	
+	$contentClass = "russell-content-full";
+	if ( has_post_thumbnail() ) {
+		
+    	$imageAttachment = wp_get_attachment_image_src( get_post_thumbnail_id(), 'russell-featured-image' );
     	if ( ! empty( $imageAttachment ) ) {
-    		?>
-    		</section>
-    		<?php
-		}
-    }
-    ?>
 
-</div>
+    	    $contentClass = "russell-content-small";
+			get_template_part( 'content', 'featured-image' );
+			
+    	}
+		
+    }
+    
+    ?>
+    <section class="<?php echo esc_attr( $contentClass ); ?> russell-content-area">
+        <div>
+    
+			<?php get_template_part( 'content', 'single' ); ?>
+
+			<?php russell_post_nav(); ?>
+
+			<?php
+				// If comments are open or we have at least one comment, load up the comment template
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+			?>
+
+		    <?php get_sidebar(); ?>
+	    <div>
+		
+	    <div class="russell-copyright">
+	        <?php russell_copyright(); ?>
+	    </div>
+		
+    </section>
+
+</div><!-- .russell-content-wrapper -->
 	
 <?php endwhile; // end of the loop. ?>
 
